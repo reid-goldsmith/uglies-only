b2054de0b5b5cff32f5c6e0fe0860bb36bbca671
reid-goldsmith <63927803+reid-goldsmith@users.noreply.github.com>
2020-06-18 13:41:00 -0500

    added a comment

--- uglies-only/app.py
+++ uglies-only/app.py
@@ -1,1 +1,7 @@
-from flask import Flask  df
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html") #this renders the template
