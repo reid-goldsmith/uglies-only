994aa978e3ef0a79b47a51c70a220387f3c7e463
reid-goldsmith <63927803+reid-goldsmith@users.noreply.github.com>
2020-06-18 15:32:24 -0500

    eh

--- uglies-only/app.py
+++ uglies-only/app.py
@@ -9,15 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/upload-image", methods=["GET", "POST"])
+@app.route("/upload-image", methods=["GET","POST"])
 def upload_image():
 
-    if request.method == "POST":
-
-        if request.files:
-            image = request.files["image"]
-            print(image)
-            return redirect(request.form.values)
+    if request.files:
+        image = request.files["image"]
+        print(image)
+        return redirect(request.url)
 
     return render_template("index.html")
 
