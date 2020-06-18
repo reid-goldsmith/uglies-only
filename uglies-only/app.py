041759329dd6ce9191b0e8d4c4616f6497f51a22
reid-goldsmith <63927803+reid-goldsmith@users.noreply.github.com>
2020-06-18 13:42:13 -0500

    Update app.py

--- uglies-only/app.py
+++ uglies-only/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #adding stuff dope dope dlk;asfjlk;adsfjslfdsakfma
 def hello_world():
     return render_template("index.html") #this renders the template
