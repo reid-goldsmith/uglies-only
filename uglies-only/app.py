062cc012c996f0b715337b938d0a120dd3e75b5a
reid-goldsmith <63927803+reid-goldsmith@users.noreply.github.com>
2020-06-19 13:06:02 -0500

    comment deleted

--- uglies-only/app.py
+++ uglies-only/app.py
@@ -4,7 +4,6 @@ from flask import request, redirect, url_for
 import os
 import cv2
 #reid is super cool
-#af;lkdskfj
 app = Flask(__name__)
 UPLOAD_FOLDER = '/Users/20goldsmithr/uglies-only/uglies-only/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
